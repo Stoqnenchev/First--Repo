9cc1c2f8ad90ce937b58bced9ef83e62282525b7
Stoqnenchev <78840903+Stoqnenchev@users.noreply.github.com>
2021-09-20 10:44:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wywedete desettichno chislo");
+            int chislo = int.Parse(Console.ReadLine());
+            string b = Convert.ToString(chislo, 2);
+            Console.WriteLine("dwyichnoto chislo e:{0}",dwyichno);
+        }
+    }
+}
